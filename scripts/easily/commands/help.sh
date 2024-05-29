9c78edd889d865cc1852cdd1d7ce51f56fd3903b
Edu <edu@rdo.blog.br>
2024-05-29 18:29:37 -0300

    update help

--- scripts/easily/commands/help.sh
+++ scripts/easily/commands/help.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
-echo "Usage: easily [start|stop|create|remove] {project}";
+echo "Usage: easily [start|stop|create|remove|db] {project}";
 echo "- start {project}\t Starts the project";
 echo "- stop {?project}\t Stops the given project or the project currently running";
 echo "- restart {?project}\t Restart the given project or the project currently running";
 echo "- create {project}\t Creates a new project";
 echo "- remove {project}\t Removes the project containers from docker";
+echo "- db [backup|restore|recreate] {project}\t Backup or restore the project database";
 echo "- help\t\t\t Shows this help message\n";
